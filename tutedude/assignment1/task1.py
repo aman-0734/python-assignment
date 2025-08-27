231df9ccbb0ea2ed979365c4e0967c2b4133afd8
aman-0734 <amanasw123@gmail.com>
2025-08-27 12:35:33 +0530

    Uploaded Task 1 and Task 2 scripts

---
+++ tutedude/assignment1/task1.py
@@ -0,0 +1,12 @@
+n1=int(input("Enter the first number: "))
+n2=int(input("Enter the second number:"))
+
+add=n1+n2
+diff=n1-n2
+prod=n1*n2
+div=n1/n2
+
+print("Addition:",add)
+print("Subtraction:",diff)
+print("Multiplication:",prod)
+print("Division:",div)
